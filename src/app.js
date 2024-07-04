49d8a32661838b9d806a59cf5b0efec3450d741a
Quiroga J <juanqi3489@gmail.com>
2024-07-04 15:01:17 -0300

    Se hizo la ruta PUT, para editar un estudiante existente

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ app.get("/students/:id", (req ,res) =>{
     const studentID = req.params.id
     const getStudent = listaEstudiantes.find((e) => e.id == +studentID)
 
+    if (!getStudent){
+        return res.status(404).json({message: "El alumno no fue encontrado"})
+    }
+
     res.json(getStudent)
 })
 
@@ -33,5 +37,26 @@ app.post("/students", (req,res) =>{
 
     res.json({message:"Estudiante agregado con éxito"})
 })
+
+app.put("/students/:id", (req,res) =>{
+    const studentID = req.params.id
+    const {fullName, age, curse} = req.body
+    const findStudent = listaEstudiantes.findIndex((e) => e.id === +studentID)
+
+    if (findStudent === -1){
+        return res.status(404).json({message: "El alumno no fue encontrado"})
+    }
+
+    if (!fullName || !age || !curse || !fullName.trim() || isNaN(age) || !curse.trim()){
+        return res.status(400).json({message:"No puede ingresar valores nulos"})
+    }
+
+    listaEstudiantes[findStudent].fullName = fullName.trim()
+    listaEstudiantes[findStudent].age = +age
+    listaEstudiantes[findStudent].curse = curse.trim()
+
+    res.json({message:"Estudiante editado con éxito", "Estudiante editado":  listaEstudiantes[findStudent]})
+})
+
 
 app.listen(4321, console.log("El servidor esta funcionando en el puerto 4321"))
